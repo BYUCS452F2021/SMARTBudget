bc7e23e8af5b55bef08bea3da888111e9e87b160
Easton <easton873@homtail.com>
2021-10-13 18:37:24 -0600

    fix up the manager and a few other things

--- app/src/main/java/com/example/smartbudget/DAO/UserDao.java
+++ app/src/main/java/com/example/smartbudget/DAO/UserDao.java
@@ -2,6 +2,10 @@ package com.example.smartbudget.DAO;
 
 import com.example.smartbudget.DAO.relational.SqlDao;
 import com.example.smartbudget.DAO.relational.DatabaseSqlManager;
+import com.example.smartbudget.Model.User;
 
 public interface UserDao {
+    boolean createUser(User user);
+    boolean updateUser(User user);
+    boolean deleteUser(String id);
 }
--- app/src/main/java/com/example/smartbudget/DAO/relational/DatabaseSqlManager.java
+++ app/src/main/java/com/example/smartbudget/DAO/relational/DatabaseSqlManager.java
@@ -1,5 +1,10 @@
 package com.example.smartbudget.DAO.relational;
 
+import com.example.smartbudget.DAO.BudgetDao;
+import com.example.smartbudget.DAO.CategoryDao;
+import com.example.smartbudget.DAO.DaoFactory;
+import com.example.smartbudget.DAO.ExpenditureDao;
+import com.example.smartbudget.DAO.UserDao;
 import com.example.smartbudget.Exceptions.DataAccessException;
 
 import java.sql.ResultSet;
@@ -7,19 +12,44 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DatabaseSqlManager {
+public class DatabaseSqlManager implements DaoFactory {
+    private static DatabaseSqlManager _instance;
     private List<SqlDao> databaseDAOs;
+    private SqlDaoFactory factory;
 
     // Constructor take in path of database to connect to and need to decide how factory sets up DAOs
     // Create tables in constructor?
-    public DatabaseSqlManager(String path) throws DataAccessException{
-        SqlDaoFactory factory = new SqlDaoFactory(path);
+
+
+    private DatabaseSqlManager(String path) throws DataAccessException{
+        factory = new SqlDaoFactory(path);
         databaseDAOs = new ArrayList<>();
         databaseDAOs.add((UserSqlDao) factory.createUserDao());
         databaseDAOs.add((BudgetSqlDao) factory.createBudgetDao());
         databaseDAOs.add((CategorySqlDao) factory.createCategoryDao());
         databaseDAOs.add((ExpenditureSqlDao) factory.createExpenditureDao());
         createTables();
+    }
+
+    private static void init() throws DataAccessException {
+        init("db.sqlite");
+    }
+
+    // Testing only, DO NOT USE IN ANY CODE YOU FIEND
+    public static void init(String dbPath) throws DataAccessException {
+        _instance = new DatabaseSqlManager(dbPath);
+    }
+
+    public static DatabaseSqlManager getInstance() {
+        if (_instance == null){
+            try {
+                init();
+            } catch (DataAccessException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return _instance;
     }
 
     public void clearTables() throws DataAccessException {
@@ -38,6 +68,26 @@ public class DatabaseSqlManager {
         for (SqlDao dao : databaseDAOs){
             dao.dropTable();
         }
+    }
+
+    @Override
+    public UserDao createUserDao() {
+        return factory.createUserDao();
+    }
+
+    @Override
+    public BudgetDao createBudgetDao() {
+        return factory.createBudgetDao();
+    }
+
+    @Override
+    public CategoryDao createCategoryDao() {
+        return factory.createCategoryDao();
+    }
+
+    @Override
+    public ExpenditureDao createExpenditureDao() {
+        return factory.createExpenditureDao();
     }
 }
 
