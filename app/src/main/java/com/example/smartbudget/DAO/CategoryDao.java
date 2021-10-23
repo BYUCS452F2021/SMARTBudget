ebc79088a35f1b85a202768c2986b3e0d5d4924d
christopherdutson <christopherdutson@gmail.com>
2021-10-22 21:48:08 -0600

    hopefully doesn't break because I couldn't test it...

--- app/src/main/java/com/example/smartbudget/DAO/CategoryDao.java
+++ app/src/main/java/com/example/smartbudget/DAO/CategoryDao.java
@@ -1,7 +1,10 @@
 package com.example.smartbudget.DAO;
 
-import com.example.smartbudget.DAO.relational.SqlDao;
-import com.example.smartbudget.DAO.relational.DatabaseSqlManager;
+import com.example.smartbudget.Model.Category;
 
 public interface CategoryDao {
+    boolean createCategory(Category category);
+    boolean updateCategory(Category category);
+    boolean deleteCategory(String id);
+    Category getCategory(String category_id);
 }
--- app/src/main/java/com/example/smartbudget/DAO/relational/CategorySqlDao.java
+++ app/src/main/java/com/example/smartbudget/DAO/relational/CategorySqlDao.java
@@ -1,7 +1,14 @@
 package com.example.smartbudget.DAO.relational;
 
+import android.database.Cursor;
+
 import com.example.smartbudget.DAO.CategoryDao;
 import com.example.smartbudget.DAO.StatementExecutor;
+import com.example.smartbudget.Exceptions.DataAccessException;
+import com.example.smartbudget.Model.Category;
+import com.example.smartbudget.Model.User;
+
+import java.util.UUID;
 
 public class CategorySqlDao extends SqlDao implements CategoryDao {
     public CategorySqlDao(StatementExecutor executor) {
@@ -12,7 +19,7 @@ public class CategorySqlDao extends SqlDao implements CategoryDao {
     public String getTableStatement() {
         return "category_id TEXT PRIMARY KEY, " +
                 "category_name TEXT, " +
-                "spending_goal INTEGER, " +
+                "allotment INTEGER, " +
                 "budget_id TEXT, " +
                 "FOREIGN KEY(budget_id) REFERENCES budget(budget_id)";
     }
@@ -20,5 +27,32 @@ public class CategorySqlDao extends SqlDao implements CategoryDao {
     @Override
     protected String getTableName() {
         return "category";
+    }
+
+    public boolean createCategory(Category category) {
+        String sql = "INSERT INTO " + getTableName() + " (category_id, category_name, allotment) " +
+                " VALUES ('" + category.getId() + "', '" + category.getName() + "', '" +
+                category.getAllotment() + "');";
+        try {
+            return insert(sql) > 0;
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean updateCategory(Category category) {
+        return false;
+    }
+    public boolean deleteCategory(String id) {
+        return false;
+    }
+    public Category getCategory(String category_id) {
+        String sql = "SELECT * FROM " + getTableName() + " WHERE category_id=\'" + category_id + "\'";
+        Cursor cursor = (Cursor) executor.executeQuery(sql);
+        cursor.moveToNext();
+        String id = cursor.getString(0);
+        String name = cursor.getString(1);
+        float allotment = cursor.getFloat(2);
+        return new Category(UUID.fromString(id), name, allotment);
     }
 }
