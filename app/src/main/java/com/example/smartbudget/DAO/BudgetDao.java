c7e7ccaf9cfdace813d242830b2dda60da4433db
Easton <easton873@homtail.com>
2021-10-26 15:22:01 -0600

    add methods to budget interface

--- app/src/main/java/com/example/smartbudget/DAO/BudgetDao.java
+++ app/src/main/java/com/example/smartbudget/DAO/BudgetDao.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface BudgetDao {
     void insert(Budget budget, User user);
     List<Budget> getBudgets(User user);
+    void delete(Budget budget);
+    void update(Budget budget, User user);
 }
